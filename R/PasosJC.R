2d9bd3785535f601a6f5efcf5a2ea8817f914dbb
JCSigala <JC@Mac-mini-de-JC.local>
2024-10-29 10:05:42 -0600

    Avanzar

--- R/PasosJC.R
+++ R/PasosJC.R
@@ -1,6 +1,6 @@
 biocthis::use_bioc_pkg_templates()
 #Falta instalar styler
 install.packages("styler")
-·Para solucionar el error que salía al inicio
+#Para solucionar el error que salía al inicio
 library(gitcreds)
 gitcreds_set()
